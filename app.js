b99c44c260385fe51485039a21c7dd15f456b109
Tanmay-31122003 <143332069+Tanmay-31122003@users.noreply.github.com>
2024-08-24 11:50:54 +0530

    add all methods

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require("cors")
 app.use(cors(
   {
     origin:["http://localhost:4200"],
-    methods:["POST","GET"],
+    methods:["POST", "GET", "PUT", "DELETE"],
     credentials:true
   }
 ));
